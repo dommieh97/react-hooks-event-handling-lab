0246db232faef892cba54efda82e49c6e3e6b031
Domenique <domogenesis77@gmail.com>
2023-07-05 12:19:26 -0500

    pass

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'
+
 // Code EyesOnMe Component Here
+function EyesOnMe ()
+{
+    const handleFocus = (event) => 
+    {
+        console.log('Good!')
+    }
+    const handleBlur = (event) =>
+    {
+        console.log('Hey! Eyes on me!')
+    }
+    return(
+    
+    <>
+    <div onFocus={handleFocus} onBlur={handleBlur}>
+     <button>Eyes on me</button>
+     </div>  
+     </>
+    
+     
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,26 @@
-// Code Keypad Component Here
+// 1.In the components/Keypad.js file, create a Keypad React component.
+// 2.In that component, render an input with the right type.
+// 3.On that input, add an event handler that listens for the change event.
+// 4.When that event fires, use console.log to print out the text 'Entering password...'.
 
-function Keypad (){
-    return (
-        <div></div>
+// Code Keypad Component Here
+import React from 'react';
+function Keypad ()
+{
+     const  handlePassword = (event) => 
+     {
+         console.log("Entering password...")
+     };
+
+    return(
+    
+    <>
+    <form onChange={handlePassword}>
+     <input type="password" />
+     </form>  
+     </>
+    
+     
     )
 }
 
